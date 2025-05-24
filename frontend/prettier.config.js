cdf932dbd99d824d408e79dff4854ab06f2e0872
fillip1984 <fillip1984@gmail.com>
2025-05-24 13:45:30 -0400

    more minor tweaks and clean up

--- frontend/prettier.config.js
+++ frontend/prettier.config.js
@@ -2,9 +2,10 @@
 
 /** @type {import('prettier').Config} */
 const config = {
-  semi: false,
-  singleQuote: true,
-  trailingComma: "all",
+  bracketSameLine: true,
+  // semi: false,
+  // singleQuote: true,
+  // trailingComma: "all",
 };
 
 export default config;
